6154bdbc573f0c046d12d1844d8630e3187c8df3
Evan Duffield <duffieldevan@gmail.com>
2023-03-05 18:13:37 -0500

    Added ability to log results, choose interface

--- README.md
+++ README.md
@@ -3,19 +3,26 @@ Python script for sniffing Steam IHS Discovery Packets.
 The script must be launched as superuser, be sure to install any dependencies as superuser.
 
 ## System Requirements
-This script uses python3, wireshark, and tshark. The following command can install each on a debian-based system.
+This script uses python3, wireshark, and tshark. The following command can install each on a Debian-based system.
 ```
 sudo apt-get install python3 wireshark tshark
 ```
+On a Red Hat/Fedora-based system, these can be installed with
+```
+sudo dnf install python3 wireshark wireshark-cli
+```
 
 ## Python Requirements
-This script uses the python libraries pyshark, html5lib, and BeautifulSoup4. The following command can install each on a debian-based system.
+This script uses the python libraries pyshark, html5lib, and BeautifulSoup4. The following command can install each on a linux system.
 ```
 sudo pip install pyshark html5lib bs4
 ```
 ## Usage
 ```
-sudo python3 steamdistill.py
+sudo python3 steamdistill.py [-h] [-w]
 ```
+options:
+  -h, --help  show this help message and exit
+  -w          Writes results a file named log.txt
 ## Screenshots
 ![Example](https://eduffield.github.io/images/screen.png)
--- steamdistill.py
+++ steamdistill.py
@@ -1,19 +1,60 @@
 #!/usr/bin/env
-import pyshark
-import requests
-import html5lib
-from bs4 import BeautifulSoup
+try:
+    import pyshark
+    from pyshark.tshark import tshark
+    import requests
+    import html5lib
+    from bs4 import BeautifulSoup
+    from datetime import datetime
+    import argparse
+except ImportError:
+    print("Error: You have missing dependencies!")
+    exit(1)
 
 class Output:
-    def __init__(self):
+    def __init__(self, writetofile=False):
         self.items = []
-        print("%-18s %-22s %-24s %-12s" % ("IP", "Hostname", "Username", "Link"))
+        self.writetofile = writetofile
+        if self.writetofile == True:
+            self.log = open("log.txt", "a")
+            startdate = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            self.log.write(startdate)
+            self.log.write("\n%-18s %-22s %-24s %-12s\n" % ("IP", "Hostname", "Username", "Link"))
+        print("\n%-18s %-22s %-24s %-12s" % ("IP", "Hostname", "Username", "Link"))
 
     def put(self, ip=None, name=None, userid=None):
         if not userid in self.items:
             self.items.append(userid)
-            username, link = checkweb(userid)
+            username, link, nohypertextlink = checkweb(userid)
+            if self.writetofile == True:
+                self.log.write(("%-18s %-22s %-24s %-12s\n" % ("[" + ip + "]", name, username, nohypertextlink)))
             print("%-18s %-22s %-24s %-12s" % ("[" + ip + "]", name, username, link))
+
+    def closefile(self):
+        self.log.write("\n\n")
+        self.log.close()
+
+def getinterface():
+    all_interfaces = tshark.get_all_tshark_interfaces_names()
+    numberofinterfaces = len(all_interfaces)
+    valid = False
+    while not valid:
+        print("Choose an interface to read:")
+        for i in range(1, numberofinterfaces+1):
+            print(f"{i}. {all_interfaces[i-1]}")
+        print(f"Choice (1-{numberofinterfaces}): ", end="")
+        usrinput = input()
+        try:
+            choiceint = int(usrinput)
+            if choiceint > 0 and choiceint < numberofinterfaces + 1:
+                    choice = all_interfaces[choiceint-1]
+                    valid = True
+            else:
+                print("Invalid input. Was you choice an integer in range?")
+        except:
+            print("Invalid input. Was you choice an integer in range?")
+    return choice
+
 
 def checkweb(userid):
     weburl = "https://steamcommunity.com/profiles/" + str(userid)
@@ -23,34 +64,34 @@ def checkweb(userid):
     soup = BeautifulSoup(page.content, "html5lib")
     username = soup.find("span", attrs = {"class": "actual_persona_name"}).text
 
-    return username, link
-
-def works():
-    out = Output()
-    cap = pyshark.LiveCapture(interface='wlp3s0')
-    for packet in cap.sniff_continuously():
-        if hasattr(packet, "steam_ihs_discovery"):
-            usrip = packet.ip.src
-
-            if hasattr(packet.steam_ihs_discovery, "body_status_hostname"):
-                usrname = packet.steam_ihs_discovery.body_status_hostname
-            if hasattr(packet.steam_ihs_discovery, "body_status_user_steamid"):
-                usrid = packet.steam_ihs_discovery.body_status_user_steamid
-            out.put(usrip, usrname, usrid)
-
-def readpack():
-    cap = pyshark.FileCapture('/home/user1/Downloads/ths.pcapng')
-    pak = cap[2832]
-
-    usrip = pak.ip.src
-    usrname = pak.steam_ihs_discovery.body_status_hostname
-    usrid = pak.steam_ihs_discovery.body_status_user_steamid
-
-    out = Output()
-    out.put(usrip, usrname, usrid)
+    return username, link, weburl
 
 if __name__ == "__main__":
-    print("Steam Distiller v0.3 -- Steam In-Home Discovery Packet Sniffer")
+    args = argparse.ArgumentParser(description="Steam Distiller v0.4 -- Steam In-Home Discovery Packet Sniffer by Evan Duffield")
+    args.add_argument("-w", dest="log", action="store_const", const=True, help="Writes results a file named log.txt")
+    logactive = False
+    logactive = args.parse_args().log
+    
+    print("Steam Distiller v0.4 -- Steam In-Home Discovery Packet Sniffer")
     print("Copyright 2022-2023 Evan Duffield")
-    print("Usage:   sudo python3 steamdistill.py\n")
-    works()
+    print("usage: steamdistill.py [-h] [-w]\n")
+
+    sniff_interface = getinterface()
+    out = Output(logactive)
+
+    try:
+        cap = pyshark.LiveCapture(interface=sniff_interface)
+        #cap = pyshark.LiveCapture(interface='wlo1')
+        for packet in cap.sniff_continuously():
+            if hasattr(packet, "steam_ihs_discovery"):
+                usrip = packet.ip.src
+
+                if hasattr(packet.steam_ihs_discovery, "body_status_hostname"):
+                    usrname = packet.steam_ihs_discovery.body_status_hostname
+                if hasattr(packet.steam_ihs_discovery, "body_status_user_steamid"):
+                    usrid = packet.steam_ihs_discovery.body_status_user_steamid
+                out.put(usrip, usrname, usrid)
+    except KeyboardInterrupt or EOFError:
+        if logactive == True:
+            out.closefile()
+        exit(0)
